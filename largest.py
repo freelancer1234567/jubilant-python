9bf810523cc6e4b9cbf23b56679530c2a591c4cc
freelancer1234567 <35969685+freelancer1234567@users.noreply.github.com>
2018-09-18 14:03:20 +0530

    largest.py

---
+++ largest.py
@@ -0,0 +1,6 @@
+#largest among 3 numbers
+#getvalue from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+ print(max(a,b,c))
