9d2b7352a9df76c2772f7c3d86a8347b8c7c6e3f
freelancer1234567 <35969685+freelancer1234567@users.noreply.github.com>
2018-09-18 12:36:51 +0530

    const_vow.py

--- const_vow.py
+++ const_vow.py
@@ -1,6 +1,6 @@
 #const_vow
-l = input("")
-if l in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % l)
+a = input("")
+if a in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % a)
 else:
-	print("%s is a consonant." % l)
+	print("%s is a consonant." % a)
