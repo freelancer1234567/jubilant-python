a99aec3d3e95079730852bb9fd3cd25ec4017143
freelancer1234567 <35969685+freelancer1234567@users.noreply.github.com>
2018-09-18 12:47:48 +0530

    alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,4 @@
+#alpha
+a = input()
+if a in ('a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x','y','z'):
+	print(a," is an alphabet.")
